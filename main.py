6f22135dcbf7033a7db8b4d6f75ed7d53e894b66
herbert0419 <devshobhan4@gmail.com>
2022-12-30 23:58:33 +0530

    main.py test changes

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from insurance.pipeline.training_pipeline import start_training_pipeline
-# from insurance.pipeline.batch_prediction import start_batch_prediction
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+from insurance.utils import get_collection_as_dataframe
+import sys,os
+from insurance.entity import config_entity
+from insurance.components.data_ingestion import DataIngestion
 
-file_path="/config/workspace/insurance_main_dataset.csv"
-print(__name__)
+
+
 if __name__=="__main__":
      try:
-          #start_training_pipeline()
-          output_file = start_training_pipeline()
-          print(output_file)
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_transformation_config  = config_entity.DataTransformationConfig(training_pipeline_config)(training_pipeline_config=training_pipeline_config)
+          print(data_transformation_config.to_dict())
+          data_ingestion = DataTransformation(data_transformation_config=data_transformation_config)
+          print(data_transformation.initiate_data_transformation())
      except Exception as e:
           print(e)
