4469a263f6aad4a683ca257672cc66c2294304c2
Subhash Dixit <subhashdixit17@gmail.com>
2023-01-08 18:56:55 +0530

    updated __init__.py

--- data_dump.py
+++ data_dump.py
@@ -2,8 +2,12 @@ import pymongo
 import pandas as pd
 import json
 from insurance.config import mongo_client
+import pymongo
 # Provide the mongodb localhost url to connect python to mongodb.
 # client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+# client = pymongo.MongoClient("mongodb+srv://subhashdixit17:Insurance17@cluster0.elq8eyt.mongodb.net/?retryWrites=true&w=majority")
+# db = client.test
 
 DATABASE_NAME = "insurance"
 COLLECTION_NAME = "premium"
@@ -20,5 +24,10 @@ if __name__=="__main__":
 
     #insert converted json to mongo db  
     # client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    # print(mongo_client)
+    # client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+    print(mongo_client)
     mongo_client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
 
--- insurance/config.py
+++ insurance/config.py
@@ -14,3 +14,21 @@ class EnvironmentVariable:
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
 TARGET_COLUMN = "expenses"
+
+print(mongo_client)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
